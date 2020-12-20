e5c037f4bdd034dacee07cb742b3aabdc76bfdb3
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2020-12-20 22:14:48 +0000

    +json

--- pokeApp/src/app/app.component.ts
+++ pokeApp/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { DataService } from './data.service';
+import { Observable } from 'rxjs';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pokeApp';
+ query: string;
+ title = 'pokeApp';
+ obspoke: Observable<Object>;
+ results: any;
+
+ constructor(public pokemon: DataService) {
+ }
+ submit(query:HTMLInputElement): void {
+ if (!query.value) {
+ return;
+ }
+ this.query = query.value;
+ this.obspoke = this.pokemon.searchPoke(this.query);
+ this.obspoke.subscribe((data) => this.results = data);
+ }
 }
