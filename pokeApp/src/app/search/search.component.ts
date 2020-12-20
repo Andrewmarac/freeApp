ebee9a3a9d40fa3e54c169fd6056d6ac7d580574
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2020-12-20 22:47:49 +0000

    transfer

--- pokeApp/src/app/search/search.component.ts
+++ pokeApp/src/app/search/search.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+ selector: 'app-root',
+ templateUrl: './search.component.html',
+ styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+ query: string;
+ title = 'pokeApp';
+ obspoke: Observable<Object>;
+ results: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+ constructor(public pokemon: DataService) {
+ }
+ submit(query:HTMLInputElement): void {
+ if (!query.value) {
+ return;
+ }
+ this.query = query.value;
+ this.obspoke = this.pokemon.searchPoke(this.query);
+ this.obspoke.subscribe((data) => this.results = data);
+ }
 }
